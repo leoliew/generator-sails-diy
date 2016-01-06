6df759d77be539827cc6b933a1e366c56068535e
Leo <lzm1093038078@gmail.com>
2016-01-07 00:25:52 +0800

    pass config generate test

--- config/steps/writing.js
+++ config/steps/writing.js
@@ -7,5 +7,7 @@
 
 module.exports = function(){
   this.directory('config', 'config');
+  this.copy('cors.template', 'config/cors.js');
   this.fs.copy(this.templatePath('LocalesReadme.md'),'config/locales/_README.md');
+  this.fs.copy(this.templatePath('csrfConfig.js'),'config/csrf.js');
 };
--- config/templates/csrfConfig.js
+++ config/templates/csrfConfig.js
@@ -54,7 +54,7 @@
 *                                                                           *
 * You may also specify more fine-grained settings for CSRF, including the   *
 * domains which are allowed to request the CSRF token via AJAX. These       *
-* settings override the general CORS settings in your config/cors.js file.  *
+* settings override the general CORS settings in your config/cors.template file.  *
 *                                                                           *
 ****************************************************************************/
 
