c83e1dc7047e388025606fcf9fcff78fa37aa73c
Leo Liu <zmliu0077@gmail.com>
2016-02-16 23:31:14 +0800

    add grunt package verion

--- src/grunt/steps/install.js
+++ src/grunt/steps/install.js
@@ -4,18 +4,18 @@
  */
 
 var DEPENDENCIES = [
-  "grunt",
-  "grunt-contrib-clean",
-  "grunt-contrib-coffee",
-  "grunt-contrib-concat",
-  "grunt-contrib-copy",
-  "grunt-contrib-cssmin",
-  "grunt-contrib-jst",
-  "grunt-contrib-less",
-  "grunt-contrib-uglify",
-  "grunt-contrib-watch",
-  "grunt-sails-linker",
-  "grunt-sync"
+  "grunt@0.4.2",
+  "grunt-contrib-clean@~0.5.0",
+  "grunt-contrib-coffee@~0.10.1",
+  "grunt-contrib-concat@~0.3.0",
+  "grunt-contrib-copy@~0.5.0",
+  "grunt-contrib-cssmin@~0.9.0",
+  "grunt-contrib-jst@~0.6.0",
+  "grunt-contrib-less@0.11.1",
+  "grunt-contrib-uglify@~0.4.0",
+  "grunt-contrib-watch@^0.6.0",
+  "grunt-sails-linker@~0.9.5",
+  "grunt-sync@~0.0.4"
 ];
 
 module.exports = function(){
